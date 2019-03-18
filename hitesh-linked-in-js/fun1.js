dd9b5d88b9768c12ea81a8f44ea69db8621383c9
Ronak <vanpariyar@gmail.com>
2019-03-18 09:59:11 +0530

    added comments

--- hitesh-linked-in-js/fun1.js
+++ hitesh-linked-in-js/fun1.js
@@ -1,3 +1,7 @@
+/*
+        This code is for the linkdin invitation accept button
+        You can paste in the browsers cosoles than it run the browser
+*/
 var x = document.getElementsByClassName('button-secondary-medium');
 for(var i = 0; i < x.length ; i++){
     x[i].click();
